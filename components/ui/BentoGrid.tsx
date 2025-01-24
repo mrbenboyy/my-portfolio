7221b5ab53525947de1ce2ea806a476e6c3678e4
mrbenboyyy <mrbenboyy@gmail.com>
2025-01-24 17:47:33 +0100

    refactor: remove unused div from BentoGridItem component

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -88,7 +88,6 @@ export const BentoGridItem = ({
                 </div>
                 {id === 6 && (
                     <BackgroundGradientAnimation>
-                        <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
                     </BackgroundGradientAnimation>
                 )}
 
